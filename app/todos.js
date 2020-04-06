bc71959715bd7a6c3dcf12add8652ea3e5035df9
Willian Anselmo <willian0506@hotmail.com>
2020-04-06 18:51:55 -0300

    Adicionado complemento para remover todos

--- app/todos.js
+++ app/todos.js
@@ -15,7 +15,20 @@ function renderTodos(){
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+
+    linkElement.setAttribute('href', '#');
+
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
@@ -31,3 +44,8 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+  todos.splice(pos, 1);
+  renderTodos();
+}
