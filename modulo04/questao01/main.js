ebed9965b7968eb87114398a321e7813160d09bd
Willian Anselmo <willian0506@hotmail.com>
2020-04-07 20:25:00 -0300

    Utilização de Promise para fixação e entendimento

--- modulo04/questao01/main.js
+++ modulo04/questao01/main.js
@@ -1,26 +1,33 @@
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
-var idade = inputElement.value;
 
-function checaIdade(idade) {
-  return new Promise(function (resolve, reject) {
-    
-    inputElement.innerHTML = '';
+var checaIdade = function() {
+  return new Promise(function(resolve, reject) {
+    var idade = inputElement.value;
 
+    console.log('aki'+ idade);
     if (idade >= 18) {
+      inputElement.value = '';
+
+      //console.log('certo');
       resolve('Maior que 18');
     } else {
+      inputElement.value = '';
+
+      //console.log('errado');
       reject('Menor que 18');
     }
   });
 }
 
-checaIdade(20)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+function verifica(){
+  checaIdade()
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.warn(error);
+    });
+}
 
-buttonElement.onclick = checaIdade;
+buttonElement.onclick = verifica;
