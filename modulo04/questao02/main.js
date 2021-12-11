50049b607bf8c4d6404bb44795831a6715c4077b
Willian Anselmo da Silva <willian0506@hotmail.com>
2021-12-11 17:27:53 -0300

    Correções de lint

--- modulo04/questao02/main.js
+++ modulo04/questao02/main.js
@@ -1,5 +1,5 @@
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
 
 function verifica() {
   var user = inputElement.value;
@@ -7,7 +7,7 @@ function verifica() {
   var minhaPromise = function () {
     return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://api.github.com/users/'+user+'/repos');
+      xhr.open("GET", "https://api.github.com/users/" + user + "/repos");
       //xhr.open('GET', 'https://api.github.com/users/willflame/repos');
       xhr.send(null);
 
@@ -16,32 +16,33 @@ function verifica() {
           if (xhr.status === 200) {
             resolve(JSON.parse(xhr.responseText));
           } else {
-            reject('Erro na requisição');
+            reject("Erro na requisição");
           }
         }
-      }
+      };
     });
-  }
-
+  };
 
   minhaPromise()
     .then(function (response) {
-      var app = document.querySelector('#app');
-      var listRepos = document.createElement('ul');
+      var app = document.querySelector("#app");
+      var listRepos = document.createElement("ul");
 
       var user = inputElement.value;
 
-      var userRepos = document.createElement('h2');
-      var userText = document.createTextNode('Lista de repositorios do usuário: '+user);
+      var userRepos = document.createElement("h2");
+      var userText = document.createTextNode(
+        "Lista de repositorios do usuário: " + user
+      );
       userRepos.appendChild(userText);
       app.appendChild(userRepos);
 
-      for(repos of response){
-        var linhaRepos = document.createElement('li');
-        var linkUrl = document.createElement('a');
+      for (repos of response) {
+        var linhaRepos = document.createElement("li");
+        var linkUrl = document.createElement("a");
 
         linkText = document.createTextNode(repos.name);
-        linkUrl.setAttribute('href', repos.html_url);
+        linkUrl.setAttribute("href", repos.html_url);
         linkUrl.appendChild(linkText);
 
         linhaRepos.appendChild(linkUrl);
--- modulo04/questao03/main.js
+++ modulo04/questao03/main.js
@@ -1,41 +1,40 @@
-var app = document.querySelector('#app');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
-var userRepos = document.querySelector('#app h2');
-var listRepos = document.querySelector('#app ul');
+var app = document.querySelector("#app");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+var userRepos = document.querySelector("#app h2");
+var listRepos = document.querySelector("#app ul");
 
 function verifica() {
   var user = inputElement.value;
 
-  var userText = document.createTextNode('Repositorios do usuário: '+user);
+  var userText = document.createTextNode("Repositorios do usuário: " + user);
   userRepos.appendChild(userText);
 
-  linhaRepos = document.createElement('li');
-  linhaText = document.createTextNode('Carregando...');
+  linhaRepos = document.createElement("li");
+  linhaText = document.createTextNode("Carregando...");
 
   linhaRepos.appendChild(linhaText);
   listRepos.appendChild(linhaRepos);
-  
+
   var minhaPromise = function () {
     return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://api.github.com/users/'+user+'/repos');
+      xhr.open("GET", "https://api.github.com/users/" + user + "/repos");
       xhr.send(null);
-      
+
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-          
           if (xhr.status === 200) {
             resolve(JSON.parse(xhr.responseText));
           } else if (xhr.status === 404) {
             reject(1);
-          } else{
-            reject('Erro na requisição');
+          } else {
+            reject("Erro na requisição");
           }
         }
-      }
+      };
     });
-  }
+  };
 
   minhaPromise()
     .then(function (response) {
@@ -45,19 +44,21 @@ function verifica() {
       var user = inputElement.value;
 
       //var userRepos = document.querySelector('h2');
-      userRepos.innerHTML = '';
-      listRepos.innerHTML = '';
+      userRepos.innerHTML = "";
+      listRepos.innerHTML = "";
 
-      var userText = document.createTextNode('Lista de repositorios do usuário: '+user);
+      var userText = document.createTextNode(
+        "Lista de repositorios do usuário: " + user
+      );
       userRepos.appendChild(userText);
       app.appendChild(userRepos);
 
-      for(repos of response){
-        var linhaRepos = document.createElement('li');
-        var linkUrl = document.createElement('a');
+      for (repos of response) {
+        var linhaRepos = document.createElement("li");
+        var linkUrl = document.createElement("a");
 
         linkText = document.createTextNode(repos.name);
-        linkUrl.setAttribute('href', repos.html_url);
+        linkUrl.setAttribute("href", repos.html_url);
         linkUrl.appendChild(linkText);
 
         linhaRepos.appendChild(linkUrl);
@@ -66,17 +67,18 @@ function verifica() {
       app.appendChild(listRepos);
     })
     .catch(function (error) {
-      if(error === 1){
-        userRepos.innerHTML = '';
-        listRepos.innerHTML = '';
+      if (error === 1) {
+        userRepos.innerHTML = "";
+        listRepos.innerHTML = "";
 
-        var userText = document.createTextNode('O usuário '+ user +' não esta cadastrado no Github ');
+        var userText = document.createTextNode(
+          "O usuário " + user + " não esta cadastrado no Github "
+        );
         userRepos.appendChild(userText);
         app.appendChild(userRepos);
-
-      }else{
-        userRepos.innerHTML = '';
-        listRepos.innerHTML = '';
+      } else {
+        userRepos.innerHTML = "";
+        listRepos.innerHTML = "";
 
         var userText = document.createTextNode(error);
         userRepos.appendChild(userText);
